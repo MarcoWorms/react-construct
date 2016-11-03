85e906b5e40af35fb2d21e178ef1492fea403ed4
Marco Guaspari Worms <marcogworms@gmail.com>
2016-11-02 23:44:28 -0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Construct, { Sprite, bullet } from 'react-construct'
+import Construct, { Sprite, bullet } from './react-construct'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      keysDown: [],
+      playerAngle: 0
+    }
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', (event) => {
+      if (this.state.keysDown.indexOf(event.key) !== -1) { return }
+      this.setState({ keysDown: [...this.state.keysDown, event.key ] })
+      this.setAngle()
+    })
+    document.addEventListener('keyup', (event) => {
+      const removeAtIndex = this.state.keysDown.indexOf(event.key)
+      this.setState({ keysDown: this.state.keysDown.filter((key, i) => removeAtIndex !== i) })
+      this.setAngle()
+    })
+  }
+  isPressed(key) {
+    return this.state.keysDown.indexOf(key) !== -1
+  }
+  setAngle() {
+    if (this.isPressed('w')) {
+      this.setState({ playerAngle: 270 })
+    }
+    if (this.isPressed('a')) {
+      this.setState({ playerAngle: 180 })
+    }
+    if (this.isPressed('s')) {
+      this.setState({ playerAngle: 90 })
+    }
+    if (this.isPressed('d')) {
+      this.setState({ playerAngle: 0 })
+    }
+  }
   render() {
     return (
       <Construct>
-        <Sprite 
-          behaviours={[ bullet ]}
-          paint={{ backgroundColor: 'red', width: '10px', height: '10px' }}
+        <Sprite
+          initial={{
+            x: 100,
+            y: 100
+          }}
+          behaviours={[
+            bullet({ speed: 200, angle: this.state.playerAngle }),
+          ]}
+          paint={{
+            backgroundColor: 'red',
+            width: '10px',
+            height: '10px'
+          }}
         />
       </Construct>
     );
--- src/react-construct/index.js
+++ src/react-construct/index.js
@@ -1,36 +1,72 @@
 import React, { Component } from 'react'
 
-
-
-
-
 export default class Construct extends Component {
   render() {
     return (
-      {this.props.children}
+      this.props.children
     )
   }
 }
 
-export class Sprite extends Component {
-  constructor() {
-    super()
+export class GameObject extends Component {
+  constructor(props) {
+    super(props)
     this.state = {
-      x: 0,
-      y: 0
+      update: this.props.behaviours.map( behaviour =>
+        behaviour.update),
+      x: this.props.initial.x || 0,
+      y: this.props.initial.y || 0,
+      angle: 0,
+      lastFrame: performance.now()
     }
-  } 
+  }
+  handleUpdate() {
+    const now = performance.now()
+    const dt = (now - this.state.lastFrame) / 1000
+    const finalState = this.state.update.reduce((nextState, update) => {
+      return update(nextState, dt)
+    }, this.state)
+    this.setState({
+      ...finalState,
+      lastFrame: now,
+      update: this.props.behaviours.map( behaviour =>
+        behaviour.update)
+    })
+    window.requestAnimationFrame(this.handleUpdate.bind(this))
+  }
+  componentDidMount() {
+    this.handleUpdate()
+  }
+}
+
+export class Sprite extends GameObject {
   render() {
     return (
-      <div style={this.props.paint}></div>
+      <div
+        style={{
+          ...this.props.paint,
+          transform: 'translate(' + this.state.x + 'px,' + this.state.y + 'px)'
+        }}
+      />
     )
   }
 }
 
-
-export function bullet {
-  update(object, angle, speed, dt) {
-    const newObject
-    return newObject 
+export function bullet ({ speed, angle }) {
+  return {
+    update(state, dt) {
+      const x = state.x + (speed * cos(angle)) * dt
+      const y = state.y + (speed * sin(angle)) * dt
+      const nextState = { ...state, x, y }
+      return nextState
+    }
   }
+}
+
+function sin (angle) {
+  return Math.sin(Math.PI * (angle/180))
+}
+
+function cos (angle) {
+  return Math.cos(Math.PI * (angle/180))
 }
