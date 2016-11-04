be82bcdc67e20b18e5c67a109e3607724cb2d1f4
Marco Guaspari Worms <marcogworms@gmail.com>
2016-11-04 02:56:30 -0200

    Add keyboard interface

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Construct, { GameObject, bullet, boundToWindow, fade, rotate } from './react-construct'
+import Construct, {
+  GameObject, KeyDown,
+  bullet, boundToWindow, fade, rotate,
+} from './react-construct'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      keysDown: [],
       playerAngle: 0
-    }
-  }
-  componentDidMount() {
-    document.addEventListener('keydown', (event) => {
-      if (this.state.keysDown.indexOf(event.key) !== -1) { return }
-      this.setState({ keysDown: [...this.state.keysDown, event.key ] })
-      this.setAngle()
-    })
-    document.addEventListener('keyup', (event) => {
-      const removeAtIndex = this.state.keysDown.indexOf(event.key)
-      this.setState({ keysDown: this.state.keysDown.filter((key, i) => removeAtIndex !== i) })
-      this.setAngle()
-    })
-  }
-  isPressed(key) {
-    return this.state.keysDown.indexOf(key) !== -1
-  }
-  setAngle() {
-    if (this.isPressed('w')) {
-      this.setState({ playerAngle: 270 })
-    }
-    if (this.isPressed('a')) {
-      this.setState({ playerAngle: 180 })
-    }
-    if (this.isPressed('s')) {
-      this.setState({ playerAngle: 90 })
-    }
-    if (this.isPressed('d')) {
-      this.setState({ playerAngle: 0 })
     }
   }
   render() {
     return (
       <Construct>
-        <p>try WASD</p>
+        <p>Try WASD</p>
+        <KeyDown when="w" do={ () => this.setState({ playerAngle: 270 }) } />
+        <KeyDown when="a" do={ () => this.setState({ playerAngle: 180 }) } />
+        <KeyDown when="s" do={ () => this.setState({ playerAngle: 90 }) } />
+        <KeyDown when="d" do={ () => this.setState({ playerAngle: 0 }) } />
         <GameObject
           initial={{
             x: 100,
--- src/react-construct/index.js
+++ src/react-construct/index.js
@@ -67,6 +67,22 @@ export class GameObject extends Component {
   }
 }
 
+export class KeyDown extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', (event) => {
+      if (event.key === this.props.when) {
+        this.props.do()
+      }
+    })
+  }
+  shouldComponentUpdate() {
+    return false
+  }
+  render() {
+    return null
+  }
+}
+
 export function bullet ({ speed, motionAngle, setAngle }) {
   return {
     update(state, dt) {
